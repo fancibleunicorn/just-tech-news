ab3d6dd98841fa73964734fdd14781ef30ace8fb
Adam Crandall <adamcrandall91@gmail.com>
2021-01-13 15:19:35 -0500

    finished 13.3

--- models/index.js
+++ models/index.js
@@ -1,5 +1,15 @@
 // File used for collecting and exporting model data
 
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Post };
--- routes/api/index.js
+++ routes/api/index.js
@@ -3,7 +3,10 @@
 const router = require('express').Router();
 
 const userRoutes =require('./user-routes');
+const postRoutes = require('./post-routes');
 
+// replaces / with specified pathway
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
