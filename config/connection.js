c34e4f0a9ddad0099e0d97848af412451030261a
Adam Crandall <adamcrandall91@gmail.com>
2021-01-14 15:49:04 -0500

    fixed type in Jaws DB

--- config/connection.js
+++ config/connection.js
@@ -7,8 +7,8 @@ require('dotenv').config();
 // create connection to our database, pass your MYSQL information for username and password
 let sequelize;
 
-if(process.env.JAWS_URL) {
-    sequelize = new Sequelize(process.env.JAWS_URL);
+if(process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
